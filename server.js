aeede068ec4ae5820673dfcf48e9ebe11302466c
Timothy Lim <timlimytn@gmail.com>
2024-12-24 10:26:33 +0800

    updated

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 // CORS Configuration
 app.use(
