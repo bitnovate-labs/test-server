7bdae7f7313dba0e69bbdf1a929e519fbf25d530
Timothy Lim <timlimytn@gmail.com>
2024-12-27 23:01:44 +0800

    updates

--- server.js
+++ server.js
@@ -24,17 +24,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: [
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-      "x-client-key",
-      "x-client-token",
-      "x-client-secret",
-      "Authorization",
-    ],
-    credentials: true,
+    credentials: false,
   })
 );
 
