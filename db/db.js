0ed5b809996eb46fb026178cb64b4f768724148b
Timothy Lim <timlimytn@gmail.com>
2024-12-27 19:37:05 +0800

    udpated

--- db/db.js
+++ db/db.js
@@ -16,7 +16,10 @@ export const db = new Pool({
   database: "rds_db_initial",
   password: "Test123321!",
   port: 5432,
-  ssl: false,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 // Test database connection
