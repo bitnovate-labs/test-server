fd69aead0802c97074a46aff5a317f00ee47fa74
Timothy Lim <timlimytn@gmail.com>
2024-12-27 19:50:21 +0800

    updated

--- db/db.js
+++ db/db.js
@@ -6,16 +6,16 @@ const { Pool } = pg;
 dotenv.config();
 
 export const db = new Pool({
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
-  user: "postgrestest",
-  host: "rds-test-database.cvcu4u4sysuk.ap-southeast-1.rds.amazonaws.com",
-  database: "rds_db_initial",
-  password: "Test123321!",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  // user: "postgrestest",
+  // host: "rds-test-database.cvcu4u4sysuk.ap-southeast-1.rds.amazonaws.com",
+  // database: "rds_db_initial",
+  // password: "Test123321!",
+  // port: 5432,
   ssl: {
     require: true,
     rejectUnauthorized: false,
