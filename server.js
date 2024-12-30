2f7ac982680b7ff267543e9e0bca612190a56870
Timothy Lim <timlimytn@gmail.com>
2024-12-30 23:56:16 +0800

    updated

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    // origin: "https://main.dbg1wx3tbcsoa.amplifyapp.com/",
+    origin: "https://main.dbg1wx3tbcsoa.amplifyapp.com/",
     // credentials: true,
   })
 );
