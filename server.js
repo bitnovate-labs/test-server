1af4577ccd0715f88c337991441dce3643afa23a
Timothy Lim <timlimytn@gmail.com>
2024-12-30 23:52:10 +0800

    updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { graphqlHTTP } from "express-graphql";
 import { schema, rootValue } from "./graphql/schema.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
@@ -24,13 +25,13 @@ app.use(
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://main.dbg1wx3tbcsoa.amplifyapp.com/",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    // origin: "https://main.dbg1wx3tbcsoa.amplifyapp.com/",
     // credentials: true,
   })
 );
 
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/items", itemRoutes);
 
 app.get("/", (req, res) =>
